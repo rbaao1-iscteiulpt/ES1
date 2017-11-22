91810cfe7f4aa4df6993c07f0ce6e79b02915f3d
pvmpa-iscteiulpt <pvmpa@iscte-iul.pt>
2017-11-22 12:28:04 +0000

    Adicionados comentários, criação do scanner de e-mails

--- src/antiSpamFilter/AntiSpamFilterProblem.java
+++ src/antiSpamFilter/AntiSpamFilterProblem.java
@@ -37,12 +37,14 @@ public class AntiSpamFilterProblem extends AbstractDoubleProblem {
 	    for (int i = 0; i < solution.getNumberOfVariables(); i++) {
 	      x[i] = solution.getVariableValue(i) ;
 	    }
-
+	    
+	    //FN
 	    fx[0] = 0.0;
 	    for (int var = 0; var < solution.getNumberOfVariables() - 1; var++) {
 		  fx[0] += Math.abs(x[0]); // Example for testing
 	    }
 	    
+	    //FP
 	    fx[1] = 0.0;
 	    for (int var = 0; var < solution.getNumberOfVariables(); var++) {
 	    	fx[1] += Math.abs(x[1]); // Example for testing
