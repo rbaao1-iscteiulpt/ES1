f354921fc169f8e41b63e190f9a597c0e38de227
rbaao1-iscteiulpt <rbaao1@iscte-iul.pt>
2017-12-04 12:59:23 +0000

    Teste Rodrigo

---
+++ src/antiSpamFilter/SolutionComparatorTest.java
@@ -0,0 +1,14 @@
+package antiSpamFilter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolutionComparatorTest {
+
+	@Test
+	public final void testCompare() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
